99b421addd103dd011963b87adad5e3cd7bb50fa
shivani dhyani <44391735+shivanidhyani@users.noreply.github.com>
2022-09-01 11:55:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
-import pandas
+#import pandas
 import snowflake.connector
+
+from urllib.error import URLError
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
